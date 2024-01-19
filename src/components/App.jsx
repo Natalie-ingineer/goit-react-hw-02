ee73a262f940c15ef90e3a50a4b853c84abd06a4
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-01-19 11:42:23 +0200

    update 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
+import { Description } from "../components/Description/Description";
+
+const Options = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return <button onClick={handleClick}>Total click - {count}</button>;
+};
+// let value = {
+//   good: 0,
+//   neutral: 0,
+//   bad: 0,
+// };
+
 const App = () => {
   return (
     <>
-      <Profile
-      //     name={userData.username}
-      //     tag={userData.tag}
-      //     location={userData.location}
-      //     image={userData.avatar}
-      //     stats={userData.stats}
-      />
+      <Description>
+        <h1>Cafe</h1>
+        <p>Text</p>
+      </Description>
+      <>
+        <Options />
+        <Options />
+        <Options />
+      </>
     </>
   );
 };
 
 export { App };
+
+{
+  /*  */
+}
